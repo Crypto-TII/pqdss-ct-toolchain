ad0253013faf7bbfa2702879d61a88531920544d
Gilbert Ndollane Dione <gilbertndollanedione@Gilberts-MacBook-Pro.local>
2024-11-19 15:55:16 +0400

    Update Dockerfile

--- pqdss-toolchain/benchmarks/install.sh
+++ pqdss-toolchain/benchmarks/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script installs and configures dudect.
+# This script to copy cycle.h into /usr/include
 
 # Make sure the script is being executed with superuser privileges.
 if [[ "${UID}" -ne 0 ]]
@@ -9,38 +9,12 @@ then
   exit 1
 fi
 
-DUDECT_SHARE_DIR=/usr/share/dudect
-
-# Download dudect.
-if ! git clone https://github.com/oreparaz/dudect.git $DUDECT_SHARE_DIR
-then
-  echo 'Dudect could not be downloaded.'
-  exit 1
-fi
+BENCHMARKS_TMP_DIR=/tmp/benchmarks
 
 
-# Build dudect.
-cd $DUDECT_SHARE_DIR || exit
-
-if ! make
-then
-  echo 'Dudect could not be built.'
-  exit 1
-fi
-
-
-# Replace all occurences of function randombytes in dudect.h by randombytes_dudect
-
-if ! sed -i "s/randombytes/randombytes_dudect/g" "$DUDECT_SHARE_DIR"/src/dudect.h
-then
-  echo "Could not replace occurences of function randombytes by randombytes_dudect in dudect.h"
-  exit 1
-fi
-
-
-if ! cp $DUDECT_SHARE_DIR/src/dudect.h /usr/include/dudect.h
+if ! cp $BENCHMARKS_TMP_DIR/cycle.h /usr/include/cycle.h
   then
-    echo "Could not find $DUDECT_SHARE_DIR/scr/dudect.h"
+    echo "Could not find $BENCHMARKS_TMP_DIR/cycle.h"
     exit 1
 fi
 
